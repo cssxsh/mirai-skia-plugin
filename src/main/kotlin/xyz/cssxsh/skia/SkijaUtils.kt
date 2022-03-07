cb58f5501087b2d953409aa68860199d8d835f10
cssxsh <cssxsh@gmail.com>
2022-03-07 11:24:10 +0800

    fix: makeFamilies

--- src/main/kotlin/xyz/cssxsh/skia/SkijaUtils.kt
+++ src/main/kotlin/xyz/cssxsh/skia/SkijaUtils.kt
@@ -5,8 +5,13 @@ import org.jetbrains.skia.*
 public fun FontMgr.makeFamilies(): Map<String, FontStyleSet> {
     val count = familiesCount
     if (count == 0) return emptyMap()
+    val families: MutableMap<String, FontStyleSet> = HashMap()
 
-    return (0 until count).associate { index ->
-        getFamilyName(index) to makeStyleSet(index)!!
+    for (index in 0 until count) {
+        val name = getFamilyName(index)
+        val styles = makeStyleSet(index) ?: throw NoSuchElementException("${this}: ${index}.${name}")
+        families[name] = styles
     }
+
+    return families
 }
