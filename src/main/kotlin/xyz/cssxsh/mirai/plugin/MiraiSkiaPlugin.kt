74a317493291d99dfcd5bc8845574aa983d65c65
cssxsh <cssxsh@gmail.com>
2022-05-02 10:12:45 +0800

    feat: ygo

--- src/main/kotlin/xyz/cssxsh/mirai/plugin/MiraiSkiaPlugin.kt
+++ src/main/kotlin/xyz/cssxsh/mirai/plugin/MiraiSkiaPlugin.kt
@@ -70,6 +70,89 @@ public object MiraiSkiaPlugin : KotlinPlugin(
                     choyen(top, bottom).makeSnapshotResource()
                         .use { resource -> subject.uploadImage(resource = resource) }
                 }
+                """^#(zzkia|pinyin)\s+((?s).+)""".toRegex() findingReply { result ->
+                    logger.info { result.value }
+                    val (_, text) = result.destructured
+
+                    zzkia(text).makeSnapshotResource()
+                        .use { resource -> subject.uploadImage(resource = resource) }
+                }
+                """^#spell\s*(\d+)?""".toRegex() findingReply { result ->
+                    logger.info { result.value }
+                    val id = result.groups[1]?.value?.toLongOrNull()
+                        ?: message.findIsInstance<At>()?.target
+                        ?: sender.id
+                    val url = "https://q.qlogo.cn/g?b=qq&nk=${id}&s=640"
+                    val file = dataFolder.resolve("${id}.jpg")
+                    if (file.exists().not()) download(urlString = url, folder = dataFolder).renameTo(file)
+                    val face = SkiaImage.makeFromEncoded(file.readBytes())
+                    val lines = message.contentToString().lines()
+                    val member = (subject as? Group)?.get(id)
+                    val name = lines.getOrNull(1)?.takeIf { it.isNotBlank() }
+                        ?: member?.remarkOrNameCardOrNick
+                        ?: senderName
+                    val description = lines.filterIndexed { index, _ -> index > 1 }.joinToString("\n")
+                        .ifBlank { (member ?: sender).queryProfile().sign }
+                    val spell = YgoCard.Spell(name = name, description = description, face = face)
+                        .render(mold = dataFolder.resolve("ygo"))
+
+                    subject.uploadImage(resource = spell.makeSnapshotResource())
+                }
+                """^#trap\s*(\d+)?""".toRegex() findingReply { result ->
+                    logger.info { result.value }
+                    val id = result.groups[1]?.value?.toLongOrNull()
+                        ?: message.findIsInstance<At>()?.target
+                        ?: sender.id
+                    val url = "https://q.qlogo.cn/g?b=qq&nk=${id}&s=640"
+                    val file = dataFolder.resolve("${id}.jpg")
+                    if (file.exists().not()) download(urlString = url, folder = dataFolder).renameTo(file)
+                    val face = SkiaImage.makeFromEncoded(file.readBytes())
+                    val lines = message.contentToString().lines()
+                    val member = (subject as? Group)?.get(id)
+                    val name = lines.getOrNull(1)?.takeIf { it.isNotBlank() }
+                        ?: member?.remarkOrNameCardOrNick
+                        ?: senderName
+                    val description = lines.filterIndexed { index, _ -> index > 1 }.joinToString("\n")
+                        .ifBlank { (member ?: sender).queryProfile().sign }
+                    val trap = YgoCard.Trap(name = name, description = description, face = face)
+                        .render(mold = dataFolder.resolve("ygo"))
+
+                    subject.uploadImage(resource = trap.makeSnapshotResource())
+                }
+                """^#monster\s*(\d+)?""".toRegex() findingReply { result ->
+                    logger.info { result.value }
+                    val id = result.groups[1]?.value?.toLongOrNull()
+                        ?: message.findIsInstance<At>()?.target
+                        ?: sender.id
+                    val url = "https://q.qlogo.cn/g?b=qq&nk=${id}&s=640"
+                    val file = dataFolder.resolve("${id}.jpg")
+                    if (file.exists().not()) download(urlString = url, folder = dataFolder).renameTo(file)
+                    val face = SkiaImage.makeFromEncoded(file.readBytes())
+                    val lines = message.contentToString().lines()
+                    val member = (subject as? Group)?.get(id)
+                    val name = lines.getOrNull(1)?.takeIf { it.isNotBlank() }
+                        ?: member?.remarkOrNameCardOrNick
+                        ?: senderName
+                    val profile = (member ?: sender).queryProfile()
+                    val race = lines.getOrNull(2)?.takeIf { it.isNotBlank() }?.split(',', ' ', '/')
+                        ?: listOfNotNull(member?.specialTitle?.ifBlank { member.permission.name }, profile.sex.name)
+                    val description = lines.filterIndexed { index, _ -> index > 2 }.joinToString("\n")
+                        .ifBlank { profile.sign }
+                    val level = (profile.qLevel / 16) + 1
+                    val attribute = YgoCard.Attribute.monster().random()
+                    val trap = YgoCard
+                        .Monster(
+                            name = name,
+                            description = description,
+                            face = face,
+                            level = level,
+                            race = race,
+                            attribute = attribute
+                        )
+                        .render(mold = dataFolder.resolve("ygo"))
+
+                    subject.uploadImage(resource = trap.makeSnapshotResource())
+                }
             }
         }
     }
