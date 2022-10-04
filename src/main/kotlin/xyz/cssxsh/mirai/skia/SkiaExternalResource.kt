e882d1984d69b87be821aaf24661bebcb9d50ec6
cssxsh <cssxsh@gmail.com>
2022-10-04 20:32:27 +0800

    fix: resource

--- src/main/kotlin/xyz/cssxsh/mirai/skia/SkiaExternalResource.kt
+++ src/main/kotlin/xyz/cssxsh/mirai/skia/SkiaExternalResource.kt
@@ -1,29 +1,18 @@
 package xyz.cssxsh.mirai.skia
 
-import io.ktor.utils.io.streams.*
-import kotlinx.coroutines.*
-import net.mamoe.mirai.internal.deps.io.ktor.utils.io.core.Input
 import net.mamoe.mirai.utils.*
 import org.jetbrains.skia.*
 import java.io.*
 
-public class SkiaExternalResource(override val origin: Data, override val formatName: String) : ExternalResource {
+public class SkiaExternalResource(override val origin: Data, override val formatName: String) :
+    ExternalResource, AbstractExternalResource({ origin.close() }) {
     public constructor(image: Image, format: EncodedImageFormat) : this(
         origin = requireNotNull(image.encodeToData(format)) { "encode $format result null." },
         formatName = format.name.replace("JPEG", "JPG")
     )
 
-    override val closed: CompletableDeferred<Unit> = CompletableDeferred()
     override val md5: ByteArray by lazy { origin.bytes.md5() }
     override val sha1: ByteArray by lazy { origin.bytes.sha1() }
     override val size: Long get() = origin.size.toLong()
-
-    override fun close() {
-        closed.completeWith(origin.runCatching { close() })
-    }
-
-    @MiraiExperimentalApi
-    override fun input(): Input = origin.bytes.inputStream().asInput().cast()
-
-    override fun inputStream(): InputStream = origin.bytes.inputStream()
+    override fun inputStream0(): InputStream = origin.bytes.inputStream()
 }
