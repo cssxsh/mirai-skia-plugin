22ff86bc062d141920ec7d8a6bf0a01c2f83f626
cssxsh <cssxsh@gmail.com>
2022-12-04 09:58:38 +0800

    update: mirai 2.13.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-    kotlin("jvm") version "1.7.20"
-    kotlin("plugin.serialization") version "1.7.20"
+    kotlin("jvm") version "1.7.21"
+    kotlin("plugin.serialization") version "1.7.21"
 
-    id("net.mamoe.mirai-console") version "2.13.0"
+    id("net.mamoe.mirai-console") version "2.13.1"
     id("me.him188.maven-central-publish") version "1.0.0-dev-3"
 }
 
@@ -27,35 +27,25 @@ repositories {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-client-okhttp:2.1.3") {
-        exclude(group = "org.jetbrains.kotlin")
-        exclude(group = "org.jetbrains.kotlinx")
-        exclude(group = "org.slf4j")
-    }
-    implementation("io.ktor:ktor-client-encoding:2.1.3") {
-        exclude(group = "org.jetbrains.kotlin")
-        exclude(group = "org.jetbrains.kotlinx")
-        exclude(group = "org.slf4j")
-    }
-    implementation("com.squareup.okhttp3:okhttp-dnsoverhttps:4.10.0") {
-        exclude(group = "org.jetbrains.kotlin")
-        exclude(group = "org.jetbrains.kotlinx")
-        exclude(group = "org.slf4j")
-    }
+    api("org.jetbrains.skiko:skiko-awt:0.7.37")
     implementation("org.apache.commons:commons-compress:1.21")
     implementation("org.tukaani:xz:1.9")
-    api("org.jetbrains.skiko:skiko-awt:0.7.37") {
-        exclude(group = "org.jetbrains.kotlin")
-        exclude(group = "org.jetbrains.kotlinx")
-    }
-    // shadowLink("org.jetbrains.skiko:skiko-awt")
     implementation("org.jsoup:jsoup:1.15.3")
-    compileOnly("net.mamoe:mirai-core-utils:2.13.0")
     compileOnly("com.google.code.findbugs:jsr305:3.0.2")
-    //
     testImplementation(kotlin("test"))
-    testImplementation("org.slf4j:slf4j-simple:2.0.3")
-    testImplementation("net.mamoe:mirai-logging-slf4j:2.13.0")
+    //
+    implementation(platform("net.mamoe:mirai-bom:2.13.1"))
+    compileOnly("net.mamoe:mirai-core-utils")
+    compileOnly("net.mamoe:mirai-console-compiler-common")
+    testImplementation("net.mamoe:mirai-logging-slf4j")
+    //
+    implementation(platform("io.ktor:ktor-bom:2.1.3"))
+    implementation("io.ktor:ktor-client-okhttp")
+    implementation("io.ktor:ktor-client-encoding")
+    implementation("com.squareup.okhttp3:okhttp-dnsoverhttps:4.10.0")
+    //
+    implementation(platform("org.slf4j:slf4j-parent:2.0.5"))
+    testImplementation("org.slf4j:slf4j-simple")
 }
 
 kotlin {
