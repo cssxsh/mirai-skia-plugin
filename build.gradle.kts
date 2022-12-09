85efdc06ae06a20ae14a544400152f2ee302c417
cssxsh <cssxsh@gmail.com>
2022-12-09 22:04:40 +0800

    update: mirai 2.13.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     kotlin("jvm") version "1.7.21"
     kotlin("plugin.serialization") version "1.7.21"
 
-    id("net.mamoe.mirai-console") version "2.13.1"
+    id("net.mamoe.mirai-console") version "2.13.2"
     id("me.him188.maven-central-publish") version "1.0.0-dev-3"
 }
 
