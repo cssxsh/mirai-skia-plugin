7f0aa803eb15d4438d132821a2358cf83f69ea47
cssxsh <cssxsh@gmail.com>
2023-01-14 17:24:45 +0800

    fix: style inline

--- src/main/kotlin/xyz/cssxsh/skia/SVGDOM.kt
+++ src/main/kotlin/xyz/cssxsh/skia/SVGDOM.kt
@@ -30,43 +30,50 @@ public fun SVGDOM.Companion.makeFromFile(xml: File, baseUri: String = ""): SVGDO
  * @see SVGDOM.Companion.makeFromString
  */
 public fun SVGDOM.Companion.makeFromXml(document: Document): SVGDOM {
-    for (style in document.select("style")) {
-        val text = style?.text() ?: continue
-        document.apply(style = text)
-        style.remove()
+    for (style in document.select("style").remove()) {
+        document.apply(css = style.text())
     }
 
     return SVGDOM(data = Data.makeFromBytes(bytes = document.toString().toByteArray()))
 }
 
+private const val CSS_FONT_REGEX =
+    """font:\s*(normal|italic|oblique|inherit|)\s*(normal|small-caps|inherit|)\s*(normal|bold|bolder|inherit|\d+|)\s+([^/ ]+)/?(\S*)\s+(.+);"""
+
 /**
- * 将 [style] 生效
+ * 将 [css] 生效
  */
-private fun Document.apply(style: String) {
+private fun Document.apply(css: String) {
     var pos = 0
 
     while (true) {
-        val before = style.indexOf('{', pos)
+        val before = css.indexOf('{', pos)
         if (before == -1) break
-        val after = style.indexOf('}', before)
+        val after = css.indexOf('}', before)
         if (after == -1) break
 
-        val query = style.substring(pos, before)
-        val attributes = style.substring(before + 1, after)
-            .splitToSequence(';')
-            .filter { it.isNotBlank() }
-            .map { it.split(':') }
+        val query = css.substring(pos, before)
+        val value = css.substring(before + 1, after)
+            .replace(CSS_FONT_REGEX.toRegex()) { match ->
+                // fixme: font: ...
+                val (style, variant, weight, size, height, family) = match.destructured
+                buildString {
+                    if (style.isNotEmpty()) append("font-style: ").append(style).append("; ")
+                    if (variant.isNotEmpty()) append("font-variant: ").append(variant).append("; ")
+                    if (weight.isNotEmpty()) append("font-weight: ").append(weight).append("; ")
+                    append("font-size: ").append(size).append("; ")
+                    if (height.isNotEmpty()) append("line-height: ").append(height).append("; ")
+                    append("font-family: ").append(family).append("; ")
+                }
+            }
         val elements = try {
             select(query)
         } catch (_: Selector.SelectorParseException) {
-            pos = after + 1
-            continue
+            emptyList()
         }
 
-        for ((key, value) in attributes) {
-            for (element in elements) {
-                element.attr(key, value)
-            }
+        for (element in elements) {
+            element.attr("style", value + element.attr("style"))
         }
 
         pos = after + 1
