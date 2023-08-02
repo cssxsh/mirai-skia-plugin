d0ce4f65299211f7c4776043829a63febac75ed1
cssxsh <cssxsh@gmail.com>
2023-08-02 11:44:21 +0800

    update: dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-    kotlin("jvm") version "1.8.10"
-    kotlin("plugin.serialization") version "1.8.10"
+    kotlin("jvm") version "1.8.22"
+    kotlin("plugin.serialization") version "1.8.22"
 
-    id("net.mamoe.mirai-console") version "2.15.0-RC"
+    id("net.mamoe.mirai-console") version "2.15.0"
     id("me.him188.maven-central-publish") version "1.0.0-dev-3"
 }
 
@@ -26,7 +26,7 @@ repositories {
 }
 
 dependencies {
-    api("org.jetbrains.skiko:skiko-awt:0.7.69")
+    api("org.jetbrains.skiko:skiko-awt:0.7.72")
     implementation("org.apache.commons:commons-compress:1.23.0")
     implementation("org.tukaani:xz:1.9")
     implementation("org.jsoup:jsoup:1.16.1")
@@ -37,11 +37,11 @@ dependencies {
     compileOnly("net.mamoe:mirai-console-compiler-common")
     testImplementation("net.mamoe:mirai-logging-slf4j")
     //
-    implementation(platform("io.ktor:ktor-bom:2.2.4"))
+    implementation(platform("io.ktor:ktor-bom:2.3.3"))
     implementation("io.ktor:ktor-client-okhttp")
     implementation("io.ktor:ktor-client-encoding")
     //
-    implementation(platform("com.squareup.okhttp3:okhttp-bom:4.10.0"))
+    implementation(platform("com.squareup.okhttp3:okhttp-bom:4.11.0"))
     implementation("com.squareup.okhttp3:okhttp-dnsoverhttps")
     //
     implementation(platform("org.slf4j:slf4j-parent:2.0.7"))
