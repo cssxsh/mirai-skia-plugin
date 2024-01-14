a82f890da7925c3b4caffbc4e884126fbc21897b
cssxsh <cssxsh@gmail.com>
2024-01-14 12:00:49 +0800

    update: dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     kotlin("jvm") version "1.8.22"
     kotlin("plugin.serialization") version "1.8.22"
 
-    id("net.mamoe.mirai-console") version "2.15.0"
+    id("net.mamoe.mirai-console") version "2.16.0"
     id("me.him188.maven-central-publish") version "1.0.0-dev-3"
 }
 
@@ -26,25 +26,25 @@ repositories {
 }
 
 dependencies {
-    api("org.jetbrains.skiko:skiko-awt:0.7.72")
-    implementation("org.apache.commons:commons-compress:1.23.0")
+    api("org.jetbrains.skiko:skiko-awt:0.7.90")
+    implementation("org.apache.commons:commons-compress:1.25.0")
     implementation("org.tukaani:xz:1.9")
-    implementation("org.jsoup:jsoup:1.16.1")
+    implementation("org.jsoup:jsoup:1.17.2")
     testImplementation(kotlin("test"))
     //
-    implementation(platform("net.mamoe:mirai-bom:2.15.0"))
+    implementation(platform("net.mamoe:mirai-bom:2.16.0"))
     compileOnly("net.mamoe:mirai-core-utils")
     compileOnly("net.mamoe:mirai-console-compiler-common")
     testImplementation("net.mamoe:mirai-logging-slf4j")
     //
-    implementation(platform("io.ktor:ktor-bom:2.3.3"))
+    implementation(platform("io.ktor:ktor-bom:2.3.7"))
     implementation("io.ktor:ktor-client-okhttp")
     implementation("io.ktor:ktor-client-encoding")
     //
-    implementation(platform("com.squareup.okhttp3:okhttp-bom:4.11.0"))
+    implementation(platform("com.squareup.okhttp3:okhttp-bom:4.12.0"))
     implementation("com.squareup.okhttp3:okhttp-dnsoverhttps")
     //
-    implementation(platform("org.slf4j:slf4j-parent:2.0.7"))
+    implementation(platform("org.slf4j:slf4j-parent:2.0.11"))
     testImplementation("org.slf4j:slf4j-simple")
 }
 
